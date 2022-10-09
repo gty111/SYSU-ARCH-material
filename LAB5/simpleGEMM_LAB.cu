ce8f0d004c4793b04fd5cccf5853bc39ba1f2b4b
gtyinstinct <guoty9@mail2.sysu.edu.cn>
2022-10-09 21:48:36 +0800

    update simpleGEMM

--- LAB5/simpleGEMM_LAB.cu
+++ LAB5/simpleGEMM_LAB.cu
@@ -1,216 +1,166 @@
-#include <cassert>
+/**
+ * @author gtyinstinct
+ * naive GEMM(int) implemention for demo
+ * C = AB
+ * A  (M X K) 
+ * B  (K X N)
+ * C  (M X N)
+ **/
+
+#include <cstdlib>
 #include <cstdio>
-#include <functional>
-#include <vector>
 #include <random>
+#include <functional>
 
-#define PAD(X,Y) (X % Y ? (X/Y+1)*Y : X)
+#define M 128               // matrix shape M in GEMM
+#define N 64                // matrix shape N in GEMM
+#define K 256               // matrix shape K in GEMM
+#define RANGE 256           // data range is [-RANGE/2,RANGE/2)
+#define TEST_TIMES 2        // iteration times
+#define BLOCK_DIM_MIN 128   // minimum of BLOCK_DIM
+#define BLOCK_DIM_MAX 1024  // maximum of BLOCK_DIM 
+#define USE_TIMER 1         // 1 means use timer; 0 means without timer
 
-#define BLOCK_DIM_DEFAULT 32
-#define K_DEFAULT 128
-#define TEST_TIME 1
-
-template <typename TIN,typename TOUT,
-      int M_TILE,int N_TILE>
-__global__ void gemm_kernel(TIN *a, TIN *b, TOUT *c,
-      int M_PAD,int N_PAD,int K_PAD) {
-    
-    // K_DEFAULT equals K_PAD
-    __shared__ TIN a_tile[M_TILE][K_DEFAULT];
-    __shared__ TIN b_tile[K_DEFAULT][N_TILE];
-
-    const int ndim = N_PAD/N_TILE;
-    const int midx = blockIdx.x / ndim;
-    const int nidx = blockIdx.x % ndim;
-
-    //copy a_tile
-    for(int i=threadIdx.x;i<M_TILE*K_PAD;i+=blockDim.x){
-        a_tile[i/K_PAD][i%K_PAD] = a[(i/K_PAD+midx*M_TILE)*K_PAD+i%K_PAD];
+// kernel function
+// implement GEMM C=AB
+__global__ void 
+GEMM_gpu(int *A,int *B,int *C){
+    // index in Grid scope
+    int idx = blockDim.x * blockIdx.x + threadIdx.x;
+    // row index in C
+    int row = idx / N;
+    // column index in C
+    int col = idx % N;
+    // redundant thread do nothing
+    if(row>=M)return;
+    // init accumulation
+    int sum = 0;
+    // calculate C[row][col]
+    for(int k=0;k<K;k++){
+        sum += A[row*K + k] * B[k*N + col];
     }
-
-    //copy b_tile
-    for(int i=threadIdx.x;i<K_PAD*N_TILE;i+=blockDim.x){
-        b_tile[i/N_TILE][i%N_TILE] = b[i/N_TILE*N_PAD+i%N_TILE+nidx*N_TILE];
-    }
-    
-    __syncthreads();
-    //cal
-    TOUT acc;
-    const int base = midx*N_PAD*M_TILE + nidx*N_TILE;
-    for(int i=threadIdx.x;i<M_TILE*N_TILE;i+=blockDim.x){
-        int row = i/N_TILE;
-        int col = i%N_TILE;
-        acc = 0;
-        for(int j=0;j<K_PAD;j++){
-            acc += a_tile[row][j]*b_tile[j][col];
-        }
-        c[row*N_PAD+col+base] = acc;
-    }
+    // write result to C
+    C[row*N + col] = sum;
 }
 
-template <typename T> struct cuda_vector {
-    T *device_data;
-    std::vector<T> host_data;
-    cuda_vector(size_t n) : host_data(n,0) {
-        cudaMalloc(&device_data, sizeof(T) * n);
-    }
-    ~cuda_vector() { cudaFree(device_data); }
-    void sync_device() {
-        cudaMemcpy(device_data, host_data.data(), sizeof(T) * host_data.size(),
-                    cudaMemcpyHostToDevice);
-    }
-    void sync_host() {
-        cudaMemcpy(host_data.data(), device_data, sizeof(T) * host_data.size(),
-                    cudaMemcpyDeviceToHost);
-    }
-    void print(int dim) {
-    printf("---------------\n");
-    for(int i=0;i<host_data.size()/dim;i++){
-        for(int j=0;j<dim;j++){
-            std::printf("%4.0f",(float)host_data[i*dim+j]);
-            if(j==dim-1) std::printf("\n");
-            else std::printf(" ");
-        }
-    }
-    }
-};
-
-
-enum DIR {ARR2VECTOR,VECTOR2ARR};
-
-template <typename TARR,typename TVEC,DIR dir>
-void gemm_copy(int ARR_M,int ARR_N,TARR *arr,
-      int VEC_M,int VEC_N,cuda_vector<TVEC> &vec){
-    assert(VEC_M>=ARR_M && VEC_N>=ARR_N);
-    if(dir==ARR2VECTOR){
-        for(int i=0;i<ARR_M;i++)
-        for(int j=0;j<ARR_N;j++){
-            vec.host_data[i*VEC_N+j] = arr[i*ARR_N+j];
-        }
-    }else if(dir==VECTOR2ARR){   
-        for(int i=0;i<ARR_M;i++){
-            for(int j=0;j<ARR_N;j++){
-            arr[i*ARR_N+j] = vec.host_data[i*VEC_N+j];
+bool
+check(int *C_cpu,int *C_gpu){
+    for(int i=0;i<M;i++){
+        for(int j=0;j<N;j++){
+            if(C_cpu[i*N + j] != C_gpu[i*N + j]){
+                printf("at (%d,%d) expect %d but got %d\n",i,j,C_cpu,C_gpu);
+                return 0;
             }
-        }
-    }else assert(0);
-}
-
-void Timer(const char *tag, const std::function<void()> &kernel,
-               int test_time = TEST_TIME) {
-    float min_time = 9e99;
-    for (int i = 0; i < test_time; ++i) {
-        cudaEvent_t beg, end;
-        cudaEventCreate(&beg);
-        cudaEventCreate(&end);
-        cudaEventRecord(beg);
-        kernel();
-        cudaEventRecord(end);
-        cudaEventSynchronize(beg);
-        cudaEventSynchronize(end);
-        float elapsed_time;
-        cudaEventElapsedTime(&elapsed_time, beg, end);
-        min_time = std::min(min_time, elapsed_time);
-        std::printf("[%s] iter %d: %f ms elapsed, %f ms min.\n", tag, i,
-                    elapsed_time, min_time);
-    }
-}
-
-template <typename TIN, typename TOUT,int M_TILE,int N_TILE>
-void simple_GEMM(int M,int N,int K,TIN *a_in,TIN *b_in,TOUT *c_out){
-    assert(M!=0 && N!=0 && K!=0);
-
-    const int M_PAD = PAD(M,M_TILE) ;
-    const int N_PAD = PAD(N,N_TILE) ;
-    const int K_PAD = K ;
-
-    assert(K_PAD==K_DEFAULT);
-
-    cuda_vector<TIN> a(M_PAD*K_PAD),b(K_PAD*N_PAD);
-    cuda_vector<TOUT> c(M_PAD*N_PAD);
-
-    //init a b
-    gemm_copy<TIN,TIN,ARR2VECTOR>(M,K,a_in,M_PAD,K_PAD,a);
-    gemm_copy<TIN,TIN,ARR2VECTOR>(K,N,b_in,K_PAD,N_PAD,b);
-
-    // sync to device
-    a.sync_device();
-    b.sync_device();
-
-    
-    int GRID_DIM,BLOCK_DIM;
-    BLOCK_DIM = BLOCK_DIM_DEFAULT;
-    GRID_DIM = (M_PAD/M_TILE) * (N_PAD/N_TILE);
-    printf("GRID_DIM:%d BLOCK_DIM:%d\n",GRID_DIM,BLOCK_DIM);
-    Timer("simpleGEMM", [&]{
-    gemm_kernel<TIN,TOUT,M_TILE,N_TILE>
-        <<<GRID_DIM,BLOCK_DIM>>>(a.device_data,b.device_data,c.device_data,
-            M_PAD,N_PAD,K_PAD);});
-    
-
-    c.sync_host();
-
-    gemm_copy<TOUT,TOUT,VECTOR2ARR>(M,N,c_out,M_PAD,N_PAD,c);
-}
-
-template<typename TIN,typename TOUT>
-bool valid(int M,int N,int K,TIN *a,TIN *b,TOUT *c,double threshold=1e-3){
-    for(int i=0;i<M;i++)
-    for(int j=0;j<N;j++){
-        TOUT val = 0;
-        for(int p=0;p<K;p++){
-            val += a[i*K+p] * b[p*N+j];
-        }
-        TOUT abs = val>c[i*N+j] ? val-c[i*N+j] : c[i*N+j] - val;
-        if(abs/val>threshold){
-            printf("at %d %d ",i,j);
-            printf("expect:%f got:%f relative error:%f%%(>%f%%)\n",
-            (float)val,(float)c[i*N+j],(float)abs/val*100,threshold*100);
-            return 0;
         }
     }
     return 1;
 }
 
-template<typename T>
-void initVal(int size,T* arr){
-    std::random_device rd;
-    std::default_random_engine eng(rd());
-    std::uniform_real_distribution<float> distr(-1e1, 1e1);
-    for(int i=0;i<size;i++){
-        arr[i] = distr(eng);
+void
+GEMM_cpu(int *A,int *B,int *C){
+    for(int i=0;i<M;i++){
+        for(int j=0;j<N;j++){
+            int sum = 0;
+            for(int k=0;k<K;k++){
+                sum += A[i*K + k] * B[k*N + j];
+            }
+            C[i*N +j] = sum;
+        }
     }
 }
 
-template<typename TIN,typename TOUT>
-void benchmark(int M,int N,int K,bool ifcheck=1){
-    printf("----------------\n");
+void 
+initVal(int *arr,size_t size){
+    for(int i=0;i<size;i++){
+        arr[i] = rand() % RANGE - RANGE/2; // [-RANGE/2,RANGE/2)
+    }
+}
+
+void Timer(const char *tag, const std::function<void()> &kernel,
+            int test_time=1) {
+  for (int i = 0; i < test_time; ++i) {
+    cudaEvent_t beg, end;
+    cudaEventCreate(&beg);
+    cudaEventCreate(&end);
+    cudaEventRecord(beg);
+    kernel();
+    cudaEventRecord(end);
+    cudaEventSynchronize(beg);
+    cudaEventSynchronize(end);
+    float elapsed_time;
+    cudaEventElapsedTime(&elapsed_time, beg, end);
+    printf("[%s] iter %d: %f ms elapsed\n", tag, i,elapsed_time);
+  }
+}
+
+int 
+main(){
+  #if USE_TIMER==1
+    printf("Timer:ON\n");
+  #else
+    printf("Timer:OFF\n");
+  #endif 
     printf("M:%d N:%d K:%d\n",M,N,K);
 
-    srand(time(NULL));
+    // set magic number for seed
+    srand(8);
 
-    TIN *a,*b;
-    TOUT *c;
+    // malloc mem for A B C 
+    // h:host pointer,d:device pointer
+    int *A_h,*A_d,*B_h,*B_d,*C_h,*C_d;
+    size_t A_size,B_size,C_size;
+    // calculate size(byte) for A B C
+    A_size = M*K*sizeof(int);
+    B_size = K*N*sizeof(int);
+    C_size = M*N*sizeof(int);
+    // malloc on host CPU (row major)
+    A_h = (int *)malloc(A_size);
+    B_h = (int *)malloc(B_size);
+    C_h = (int *)malloc(C_size);
+    // malloc on device GPU (row major)
+    cudaMalloc(&A_d,A_size);
+    cudaMalloc(&B_d,B_size);
+    cudaMalloc(&C_d,C_size);
 
-    a = (TIN*) malloc(M*K*sizeof(TIN));
-    b = (TIN*) malloc(K*N*sizeof(TIN));
-    c = (TOUT*)malloc(M*N*sizeof(TOUT));
+    // init val for A B at host memory
+    initVal(A_h,A_size/sizeof(int));
+    initVal(B_h,B_size/sizeof(int));
 
-    initVal<TIN>(M*K,a);
-    initVal<TIN>(K*N,b);
+    // copy A B from host to device memory
+    cudaMemcpy(A_d,A_h,A_size,cudaMemcpyHostToDevice);
+    cudaMemcpy(B_d,B_h,B_size,cudaMemcpyHostToDevice);
 
-    simple_GEMM<TIN,TOUT,16,16>(M,N,K,a,b,c);
+    // launch kernel on GPU
+    int GridDim,BlockDim;
+    BlockDim = min(BLOCK_DIM_MAX,N); // let BlockDim <= BLOCK_DIM_MAX
+    BlockDim = max(BLOCK_DIM_MIN,BlockDim); // let BlockDim >= BLOCK_DIM_MIN
+    GridDim = M*N/BlockDim; // launch thread num M*N 
+    GridDim = max(1,GridDim); // let GridDim >= 1
+    printf("GridDim:%d BlockDim:%d\n",GridDim,BlockDim);
+  #if USE_TIMER==1
+    Timer("GEMM on GPU",[&]{GEMM_gpu<<<GridDim,BlockDim>>>(A_d,B_d,C_d);}
+    ,TEST_TIMES);
+  #else
+    GEMM_gpu<<<GridDim,BlockDim>>>(A_d,B_d,C_d); 
+  #endif
+    
 
-    if(ifcheck && valid<TIN,TOUT>(M,N,K,a,b,c,1e-2)){
-        std::printf("check pass\n");
-    }else if(ifcheck){
-        std::printf("check fail\n");
+    // copy C from device to host memory
+    cudaMemcpy(C_h,C_d,C_size,cudaMemcpyDeviceToHost);
+
+    // Test GEMM on CPU
+    int *C_cpu = (int *)malloc(C_size);
+  #if USE_TIMER==1
+    Timer("GEMM on CPU",[&]{GEMM_cpu(A_h,B_h,C_cpu);}
+    ,TEST_TIMES);
+  #else 
+    GEMM_cpu(A_h,B_h,C_cpu);
+  #endif
+
+    // check result
+    if(check(C_cpu,C_h)){
+        printf("check pass!\n");
     }else{
-        std::printf("skip check\n");
+        printf("check fail\n");
     }
-   
-}
-
-int main(){
-    benchmark<double,double>(128,128,K_DEFAULT,1);
 }
